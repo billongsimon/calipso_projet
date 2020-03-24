89e9730302e1449ef143e15d4008b327a6e8f97e
simon <simonstevenbillong@gmail.com>
2020-03-24 20:22:44 +0100

    creation des3 boutons create del modify

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -42,8 +42,6 @@ class AdminController extends AbstractController
     {
         $repo=$this->getDoctrine() ->getRepository(Page::class);
         $page=$repo->findAll();
-        
-            
              
         return $this->render('admin/page.html.twig', [
             'controller_name' => 'AdminController',
@@ -63,5 +61,82 @@ class AdminController extends AbstractController
         'page'=>$page
             ]);
     }
+
+    /**
+     * @Route("/admin/form/page", name="admin.form.page")
+     */
+    public function pageForm(Request $request, ObjectManager $manager)
+    {
+        $page =new Page();
+        $form = $this->createFormBuilder($page)
+        ->add('titre')
+        ->add('auteur')
+        ->add('createdAt')
+        ->add('jourAt')
+        ->add('contenu')
+        ->add('categorie', EntityType::class, [
+            'class' => Categorie::class,
+            "choice_label" => 'titre'
+      ])
+         
+        ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+        $manager->persist($page); 
+        $manager->flush();
+        return $this->redirectToRoute('admin.page', 
+        ['id'=>$page->getId()]); // Redirection vers la page
+        }
+        return $this->render('admin/pageform.html.twig', [
+            'formPage' => $form->createView()
+        ]);
+    }
+
+
+    /**
+    * @Route("/admin/page/{id}", name="admin.page.modif")
+    */
+    
+    public function pageModif(page $page, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createFormBuilder($page)
+        ->add('titre')
+        ->add('auteur')
+        ->add('createdAt')
+        ->add('jourAt')
+        ->add('contenu')
+        ->add('categorie', EntityType::class, [
+            'class' => Categorie::class,
+            "choice_label" => 'titre'
+    ])
+         
+        ->getForm();
+        $form->handleRequest($request);
+                
+        if($form->isSubMitted() && $form->isValid()){
+            $manager->persist($page);
+            $manager->flush();
+
+            return $this->redirectToRoute('admin.page', 
+            ['id'=>$page->getId()]); // Redirection vers la page
+        }
+        return $this->render('admin/pagemodif.html.twig', [
+               'formModifPage' => $form->createView()
+               ]);
+    }
+    /**
+    * @Route("/admin/page/{id}/deletart", name="admin.page.sup")
+    */
+    
+    public function pageSup($id, ObjectManager $manager, Request $request)
+    {
+        $repo = $this->getDoctrine()->getRepository(Page::class);
+        $page = $repo->find($id);
+
+        $manager->remove($page);
+        $manager->flush();
+        
+        return $this->redirectToRoute('admin.page');
+    } 
 
 }
